14fc034a3231981b6b8137ebaa359c9f4b2e8d25
Megan C. Smith <megan.cs601@gmail.com>
2022-12-14 08:33:42 -0500

    change imageContainer's height on onChange

--- src/Carousel.js
+++ src/Carousel.js
@@ -2,19 +2,49 @@ import { Carousel } from "react-responsive-carousel";
 import { demoImages } from "./demoImages";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./css/Carousel.css";
+import { useEffect, useRef, useState } from "react";
 
 export default function ImageCarousel() {
+  const carouselRef = useRef();
+  const [maxImageHeight, setMaxImageHeight] = useState("auto");
+
+  const checkImageSize = (index, img) => {
+    const width = img.props.children.props.width;
+    const height = img.props.children.props.height;
+    console.log("clicked");
+    if (width < height) {
+      const parentHeight = document.querySelector(".App-Content").clientHeight;
+
+      setMaxImageHeight(parentHeight);
+      return;
+    }
+
+    setMaxImageHeight("auto");
+  };
+
+  useEffect(() => {
+    //automatically adjust first image's height
+    carouselRef.current.click();
+  }, []);
+
   return (
-    <div id="carouselContainer">
+    <>
       <Carousel
         dynamicHeight={true}
         showThumbs={false}
         emulateTouch={true}
         infiniteLoop={true}
         showStatus={false}
+        onChange={checkImageSize}
+        onClickItem={checkImageSize}
       >
         {demoImages.map((image, index) => (
-          <div key={index} className="imageContainer">
+          <div
+            ref={carouselRef}
+            key={index}
+            className="imageContainer"
+            style={{ height: maxImageHeight }}
+          >
             <img
               alt={image.alt}
               src={image.src}
@@ -24,6 +54,6 @@ export default function ImageCarousel() {
           </div>
         ))}
       </Carousel>
-    </div>
+    </>
   );
 }
