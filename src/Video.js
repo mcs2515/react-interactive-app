2a810987d4ce476dbcdc61d9c48e7a7909e66653
Megan C. Smith <megan.cs601@gmail.com>
2023-01-13 10:51:57 -0500

    camelCase fixes

--- src/Video.js
+++ src/Video.js
@@ -8,9 +8,8 @@ export default function Video() {
         height="315"
         src="https://www.youtube.com/embed/lAIGb1lfpBw"
         title="YouTube video player"
-        frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
+        allowFullScreen
       ></iframe>
     </div>
   );
